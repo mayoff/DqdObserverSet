47329e8819e1f0b4fa46141265163d7ba2da4fca
Rob Mayoff <git@rob.dqd.com>
2012-08-11 02:26:21 -0500

    tweaks to fit on one page

--- DqdObserverSet/DqdObserverSet.h
+++ DqdObserverSet/DqdObserverSet.h
@@ -1,12 +1,12 @@
 @interface DqdObserverSet : NSObject
 
-- (id)initWithProtocol:(Protocol *)protocol;
+- (id)initWithProtocol:(Protocol *)protocol;	// Configure me to send messages defined in this protocol.
 
 @property (nonatomic, strong, readonly) Protocol *protocol;
 
-- (void)addObserver:(id)observer;
-- (void)removeObserver:(id)observer;
+- (void)addObserver:(id)observer;	// Add an observer that should receive protocol messages.
+- (void)removeObserver:(id)observer;	// Remove an observer that should no longer receive protocol messages.
 
-@property (nonatomic, strong) id proxy;
-
+@property (nonatomic, strong, readonly) id proxy;	// This object forwards any message in the protocol to all observers
+    // that implement it.
 @end
--- DqdObserverSet/DqdObserverSet.m
+++ DqdObserverSet/DqdObserverSet.m
@@ -2,20 +2,23 @@
 #import <objc/runtime.h>
 
 @interface DqdObserverSetMessageProxy : NSObject
-@property (nonatomic, unsafe_unretained) DqdObserverSet *observerSet;
+@property (nonatomic,unsafe_unretained) DqdObserverSet *observerSet;
 @end
 
 @implementation DqdObserverSet {
     NSMutableSet *observers_;
     NSMutableSet *pendingObservers_;
-    DqdObserverSetMessageProxy *_proxy_cached;
 }
 
 #pragma mark - Public API
 
+@synthesize proxy = _proxy;
+
 - (id)initWithProtocol:(Protocol *)protocol {
     if (self = [super init]) {
         _protocol = protocol;
+        _proxy = [[DqdObserverSetMessageProxy alloc] init];
+        [_proxy setObserverSet:self];
     }
     return self;
 }
@@ -23,9 +26,10 @@
 - (void)addObserver:(id)observer {
     if (!observers_) {
         // Create a non-retaining set.
-        observers_ = CFBridgingRelease(CFSetCreateMutable(NULL, 0, &(CFSetCallBacks){
-            .equal = kCFTypeSetCallBacks.equal,
-            .hash = kCFTypeSetCallBacks.hash
+        observers_ = CFBridgingRelease(CFSetCreateMutable(NULL, 0,
+            &(CFSetCallBacks){
+                .equal = kCFTypeSetCallBacks.equal,
+                .hash = kCFTypeSetCallBacks.hash
         }));
     }
     [observers_ addObject:observer];
@@ -36,18 +40,12 @@
     [pendingObservers_ removeObject:observer];
 }
 
-- (id)proxy {
-    if (!_proxy_cached) {
-        _proxy_cached = [[DqdObserverSetMessageProxy alloc] init];
-        _proxy_cached.observerSet = self;
-    }
-    return _proxy_cached;
-}
-
 #pragma mark - DqdObserverSetMessageProxy API
 
 - (void)forwardInvocationToObservers:(NSInvocation *)invocation {
-    pendingObservers_ = CFBridgingRelease(CFSetCreateMutableCopy(NULL, 0, (__bridge CFTypeRef)observers_));
+    pendingObservers_ = CFBridgingRelease(
+        CFSetCreateMutableCopy(NULL, 0,
+            (__bridge CFTypeRef)observers_));
     while (pendingObservers_.count > 0) {
         id observer = pendingObservers_.anyObject;
         [pendingObservers_ removeObject:observer];
@@ -55,15 +53,17 @@
             [invocation invokeWithTarget:observer];
         }
     }
-    pendingObservers_ = nil;
 }
 
 - (NSMethodSignature *)protocolMethodSignatureForSelector:(SEL)selector {
-    struct objc_method_description description = protocol_getMethodDescription(_protocol, selector, YES, YES);
+    struct objc_method_description description =
+        protocol_getMethodDescription(_protocol, selector, YES, YES);
     if (!description.name) {
-        description = protocol_getMethodDescription(_protocol, selector, NO, YES);
+        description = protocol_getMethodDescription(_protocol, selector,
+            NO, YES);
     }
-    return [NSMethodSignature signatureWithObjCTypes:description.types];
+    return [NSMethodSignature
+        signatureWithObjCTypes:description.types];
 }
 
 @end
@@ -73,7 +73,8 @@
 @synthesize observerSet = _observerSet;
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
-    return [self.observerSet protocolMethodSignatureForSelector:aSelector];
+    return [self.observerSet
+        protocolMethodSignatureForSelector:aSelector];
 }
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
@@ -81,4 +82,3 @@
 }
 
 @end
-
