42998d1b54eb88ce5a8f5b242be1e3777c66d77a
Rob Mayoff <git@rob.dqd.com>
2012-08-10 23:04:36 -0500

    checkpoint

--- DqdObserverSet/DqdObserverSet.m
+++ DqdObserverSet/DqdObserverSet.m
@@ -1,8 +1,3 @@
-/*
-Created by Rob Mayoff on 7/30/12.
-Copyright (c) 2012 Rob Mayoff. All rights reserved.
-*/
-
 #import "DqdObserverSet.h"
 #import <objc/runtime.h>
 
@@ -10,70 +5,35 @@ Copyright (c) 2012 Rob Mayoff. All rights reserved.
 @property (nonatomic, unsafe_unretained) DqdObserverSet *observerSet;
 @end
 
-static NSMutableSet *nonRetainingSet(void) {
-    CFSetCallBacks callbacks = {
-        .version = 0,
-        .retain = NULL,
-        .release = NULL,
-        .copyDescription = kCFTypeSetCallBacks.copyDescription,
-        .equal = kCFTypeSetCallBacks.equal,
-        .hash = kCFTypeSetCallBacks.hash
-
-    };
-    return CFBridgingRelease(CFSetCreateMutable(NULL, 0, &callbacks));
-}
-
 @implementation DqdObserverSet {
     NSMutableSet *observers_;
-    NSMutableSet *pendingAdditions_;
-    NSMutableSet *pendingDeletions_;
+    NSMutableSet *pendingObservers_;
     DqdObserverSetMessageProxy *_proxy_cached;
-    BOOL isForwarding_;
 }
-
-@dynamic proxy;
 
 #pragma mark - Public API
 
-- (id)init {
-    NSLog(@"I only understand -[%@ initWithProtocol:], not -[%@ init].", self.class, self.class);
-    [self doesNotRecognizeSelector:_cmd]; abort();
-}
-
 - (id)initWithProtocol:(Protocol *)protocol {
-    if (!(self = [super init]))
-        return nil;
-
-    _protocol = protocol;
-
+    if (self = [super init]) {
+        _protocol = protocol;
+    }
     return self;
 }
 
 - (void)addObserver:(id)observer {
-    if (isForwarding_ && pendingDeletions_) {
-        [pendingDeletions_ removeObject:observer];
+    if (!observers_) {
+        // Create a non-retaining set.
+        observers_ = CFBridgingRelease(CFSetCreateMutable(NULL, 0, &(CFSetCallBacks){
+            .equal = kCFTypeSetCallBacks.equal,
+            .hash = kCFTypeSetCallBacks.hash
+        }));
     }
-    
-    __strong NSMutableSet **set = isForwarding_ ? &pendingAdditions_ : &observers_;
-
-    if (!*set) {
-        *set = nonRetainingSet();
-    }
-    [*set addObject:observer];
+    [observers_ addObject:observer];
 }
 
 - (void)removeObserver:(id)observer {
-    if (isForwarding_) {
-        if (pendingAdditions_) {
-            [pendingAdditions_ removeObject:observer];
-        }
-        if (!pendingDeletions_) {
-            pendingDeletions_ = nonRetainingSet();
-        }
-        [pendingDeletions_ addObject:observer];
-    } else {
-        [observers_ removeObject:observer];
-    }
+    [observers_ removeObject:observer];
+    [pendingObservers_ removeObject:observer];
 }
 
 - (id)proxy {
@@ -86,43 +46,24 @@ static NSMutableSet *nonRetainingSet(void) {
 
 #pragma mark - DqdObserverSetMessageProxy API
 
+- (void)forwardInvocationToObservers:(NSInvocation *)invocation {
+    pendingObservers_ = CFBridgingRelease(CFSetCreateMutableCopy(NULL, 0, (__bridge CFTypeRef)observers_));
+    while (pendingObservers_.count > 0) {
+        id observer = pendingObservers_.anyObject;
+        [pendingObservers_ removeObject:observer];
+        if ([observer respondsToSelector:invocation.selector]) {
+            [invocation invokeWithTarget:observer];
+        }
+    }
+    pendingObservers_ = nil;
+}
+
 - (NSMethodSignature *)protocolMethodSignatureForSelector:(SEL)selector {
-    NSAssert(_protocol != nil, @"%@ protocol not set", self);
     struct objc_method_description description = protocol_getMethodDescription(_protocol, selector, YES, YES);
     if (!description.name) {
         description = protocol_getMethodDescription(_protocol, selector, NO, YES);
     }
-    NSAssert(description.name, @"%@ couldn't find selector %s in protocol %s", self, sel_getName(selector), protocol_getName(_protocol));
     return [NSMethodSignature signatureWithObjCTypes:description.types];
-}
-
-- (void)forwardInvocationToObservers:(NSInvocation *)invocation {
-    NSAssert(!isForwarding_, @"%@ asked to forward a message to observers recursively", self);
-
-    isForwarding_ = YES;
-    @try {
-        SEL selector = invocation.selector;
-        for (id observer in observers_) {
-            if (pendingDeletions_ && [pendingDeletions_ containsObject:observer])
-                continue;
-            if ([observer respondsToSelector:selector]) {
-                [invocation invokeWithTarget:observer];
-            }
-        }
-    }
-    @finally {
-        isForwarding_ = NO;
-
-        if (pendingAdditions_) {
-            [observers_ unionSet:pendingAdditions_];
-            pendingAdditions_ = nil;
-        }
-        
-        if (pendingDeletions_) {
-            [observers_ minusSet:pendingDeletions_];
-            pendingDeletions_ = nil;
-        }
-    }
 }
 
 @end
